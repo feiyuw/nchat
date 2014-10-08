7df25c9f072e0edb08b07fef2ad09b81938976f2
feiyuw <feiyuw@gmail.com>
2014-10-08 16:33:39 +0800

    for issue #2 and #3

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,20 @@
 var socket = io();
 
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
+  socket.emit('message', $('#m').val());
+  $('#messages').append($('<li>').text('me: ' + $('#m').val()));
   $('#m').val('');
   return false;
 });
 
-socket.on('chat message', function(msg) {
-  $('#messages').append($('<li>').text(msg));
+socket.on('message', function(msg) {
+  $('#messages').append($('<li>').text(msg.user + ': ' + msg.body));
+});
+
+socket.on('hi', function(userName) {
+  $('#messages').append($('<li>').text('User ' + userName + ' connected!'));
+});
+
+socket.on('bye', function(userName) {
+  $('#messages').append($('<li>').text('User ' + userName + ' left!'));
 });
--- server.js
+++ server.js
@@ -3,14 +3,20 @@ var debug = require('debug')('nchat');
 function startServer(app) {
   var http = require('http').Server(app);
   var io = require('socket.io')(http);
+  var nickNames = {};
 
   io.on('connection', function(socket) {
-    io.emit('chat message', socket.id + ' connected!');
-    socket.on('chat message', function(msg) {
-      io.emit('chat message', msg);
+    socket.on('message', function(msg) {
+      if (nickNames[socket.id] == null) {
+        nickNames[socket.id] = msg;
+        socket.broadcast.emit('hi', msg);
+      } else {
+        socket.broadcast.emit('message', {'user': nickNames[socket.id], 'body': msg});
+      }
     });
     socket.on('disconnect', function() {
-      io.emit('chat message', socket.id + ' disconnected!');
+      socket.broadcast.emit('bye', nickNames[socket.id]);
+      delete(nickNames[socket.id]);
     });
   });
 
